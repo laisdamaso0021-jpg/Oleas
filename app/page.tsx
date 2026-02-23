54d4bef0ca7509b7cf7ff7355dcdb07538cd761e
laisdamaso0021-jpg <laisdamaso0021@gmail.com>
2026-02-23 10:25:25 -0500

    Create the main page for the Oleas real estate website.

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Oleas Real Estate</h1>
+      <p>Your dream home awaits!</p>
+    </div>
+  );
+};
+
+export default HomePage;
